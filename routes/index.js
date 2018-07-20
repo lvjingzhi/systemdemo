e2a5d91c761fb69d019b3b5184fb656dca1ffded
lvjingzhi <625679084@qq.com>
2018-07-20 16:01:50 +0800

    搭建了一个模板

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Express',
+      link: '<link rel=\'stylesheet\' href=\'/stylesheets/index.css\' />',
+      script: '<script src="javascripts/indexpage.js"></script>'
+  });
+
 });
 
 module.exports = router;
